c141e38a0d6c446722a3c725f4a6fba0856390f0
C0d3Pr0duC3r <mail@christopherherz.de>
2024-10-23 21:50:07 +0200

    trying to create a program that lets you play and calculate with numbers of any given base

--- CustomBase.py
+++ CustomBase.py
@@ -1,7 +1,5 @@
 import string
-
-base26 = list(range(10))+list(string.ascii_lowercase)
-print(base26)
+base26 = list(map(str, range(10))) + list(string.ascii_uppercase)
 class CustomBase:
     def __init__(self, base: int, symbols=base26):
         # base defines the numbersystem
@@ -13,12 +11,52 @@ class CustomBase:
         # prints the number to the screen
         pass
 
-    def convert_to_dec(self):
+    def convert_to_dec(self, base_number):
+
+        base_number = str(base_number)
         # converts the n-based number into a decimal
-        pass
+        decimal_value = 0
+        power = 0  # Zählt die Potenz der Basis
 
-    def convert_to_base(self):
-        # converts the number to the n-based
-        pass
+        # Gehe durch die Ziffern von rechts nach links (umgekehrte Reihenfolge)
+        for digit in reversed(base_number):
+            decimal_value += int(digit) * (self.base ** power)
+            power += 1
+
+        return decimal_value
+
+    def convert_to_base(self, dec):
+        """
+        dec // self.base = new_dec
+        dec % self.base = last_digit
+        ...
+        new_dec // self.base = 0
+        new_dec % self.base = first_digit
+        """
+
+        if dec == 0:
+            return "0"
+
+        digits = []
+        while dec > 0:
+
+            digits.append(self.symbols[dec % self.base])
+            dec //= self.base
 
 
+        return ''.join(reversed(digits))
+
+
+
+
+
+#8/2 4/2 2/2 1/2
+binary = CustomBase(2)
+
+hex = CustomBase(16)
+
+print(binary.convert_to_base(127))
+
+print(binary.convert_to_dec(1010100))
+
+print(hex.convert_to_base(1232))
